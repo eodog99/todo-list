2ce6ef2b697145ef612223285ad3418a7357afea
eodog99 <eodog@naver.com>
2024-11-12 15:39:09 +0900

    todo-list-1

---
+++ src/component/TodoForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TodoForm() {
+
+    return (
+        <div>todo폼</div>
+    );
+}
+
+export default TodoForm;
---
+++ src/component/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TodoList() {
+
+    return (
+        <div>todo리스트</div>
+    );
+}
+
+export default TodoList;
